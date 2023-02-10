0c3a490e8634819991d81da8916ef9c4329fba7b
Drake Wright <rdrakewright@gmail.com>
2023-02-10 08:42:56 -0600

    Initial Commit

---
+++ SwiftFunctionsExercise.playground/Contents.swift
@@ -0,0 +1,26 @@
+//1
+func phineasFerb(){
+    print("Hey Ferb, I know what we're gonna do today!")
+}
+phineasFerb()
+//2
+func AgentP(perry: String){
+    print(perry + perry + "Do-Bah")
+}
+AgentP(perry: "Doo-Bee-")
+
+//3
+func doof(name: String, animal: String){
+    print("Curse you, " + name + " the " + animal + "!")
+}
+
+doof(name: "perry", animal: "Platypus")
+
+// 4
+func isabella() -> String {
+    return "What'cha Doin?"
+}
+
+let question = isabella()
+print(question) // What'cha Doin?
+
